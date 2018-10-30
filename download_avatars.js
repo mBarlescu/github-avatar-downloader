d2b77340d53a2c07565ec15f3852d5df3bcac59c
Mark Barlescu <barlescu92@gmail.com>
2018-10-30 04:33:58 +0000

    progress

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secret = require('./secrets.js').GITHUB_TOKEN;
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -20,7 +21,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 
 function callback(err, result) {
-
+var contributors = [];
   if(err) {
       console.log("Errors:", err);
   }
@@ -28,8 +29,22 @@ function callback(err, result) {
   var obj = JSON.parse(result);
   obj.forEach(function (i){
     console.log(i.avatar_url);
+    contributors += i.avatar_url
   })
   // console.log("Result:", obj[0]);
+  console.log(contributors)
 }
 
 getRepoContributors("jquery", "jquery", callback);
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
