a30a8dcc84968d82a794854730a865ca1f88c1d1
Mark Barlescu <barlescu92@gmail.com>
2018-10-30 02:47:30 +0000

    commiting after updating ignore file to keep out secrets file

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,22 @@
 var request = require('request');
+var request = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + mBarlescu + "/" + github-avatar-downloader + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
